7256bb3643253d820f91cff9cdbccf1bae368150
PulseKim <PulseKim@users.noreply.github.com>
2026-02-16 17:51:51 +0000

    deploy: b0be72bd9a68c2fdbc3cbd47de2965f47dabd534

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "PulseKim/PulseKim.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
